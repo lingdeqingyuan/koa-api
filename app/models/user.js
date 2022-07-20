e2fd7dd8493d9292afb9d8c416ebd99e777a5b1c
李英杰 <yrejgnl11@163.com>
2022-07-20 10:13:37 +0800

    feat: add user model

---
+++ app/models/user.js
@@ -0,0 +1,24 @@
+const { db } = require("../../core/db");
+const { Sequelize, Model } = require('sequelize');
+
+class User extends Model {
+
+}
+
+User.init({
+  // 主键 
+  // 不能为空 不能重复
+  // 数字 禁用随机字符串
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  nickname: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password: Sequelize.STRING,
+  openid: {
+    type: Sequelize.STRING(64),
+    unique: true
+  }
+})
--- core/db.js
+++ core/db.js
@@ -11,5 +11,5 @@ const sequelize = new Sequelize(dbName, user, password, {
 })
 
 module.exports = {
-  sequelize
+  db: sequelize
 }
