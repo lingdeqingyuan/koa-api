cc38645f4c34e301315d29f6080eba1709af1026
李英杰 <yrejgnl11@163.com>
2022-07-20 09:50:33 +0800

    feat: add sequelize config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  env: 'dev'
+  env: 'dev',
+  database: {
+    dbName: 'koa-api',
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'aidemoli1'
+  }
 }
